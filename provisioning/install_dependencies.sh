08c68961de806e5fcc654f538a567617b26af578
Brandon Liu <bdon@bdon.org>
2014-07-07 13:25:27 -0700

    dont use npm install globally

--- provisioning/install_dependencies.sh
+++ provisioning/install_dependencies.sh
@@ -4,4 +4,5 @@ sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
 sudo apt-get install -y nodejs make mapnik-utils nginx git unzip gdal-bin zip
-sudo npm install -g tessera carto millstone js-yaml
+cd ~/
+npm install -g tilelive-mapnik tessera carto millstone js-yaml
--- provisioning/install_services.sh
+++ provisioning/install_services.sh
@@ -6,11 +6,11 @@ set -e
 echo "DATABASE_URL: $DATABASE_URL"
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo cp tessera.nginx /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/tessera.nginx /etc/nginx/sites-enabled/tessera.conf
+sudo ln -f -s /etc/nginx/sites-available/tessera.nginx /etc/nginx/sites-enabled/tessera.conf
 sudo cp tessera.conf /etc/init/
 pushd ..
 cat project.yml | node jsonify.js $DATABASE_URL
-millstone project.mml > project_milled.mml
-carto project_milled.mml > project.xml
+~/node_modules/.bin/millstone project.mml > project_milled.mml
+~/node_modules/.bin/carto project_milled.mml > project.xml
 sudo service tessera restart
 sudo service nginx restart
