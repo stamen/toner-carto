5ff61c2f2f92f277c100f2ac97d43b8ab46b8d8e
Seth Fitzsimmons <seth@mojodna.net>
2014-07-10 13:40:29 -0700

    Self-documenting

--- highroad.sql
+++ highroad.sql
@@ -163,6 +163,38 @@ CREATE VIEW highroad_z15plus AS
          OR railway IN ('rail', 'tram', 'light_rail', 'narrow_guage', 'monorail')
       ORDER BY explicit_layer ASC, implied_layer ASC, priority DESC);
 
+CREATE OR REPLACE FUNCTION zoom(scaleDenominator numeric) RETURNS int AS $$
+BEGIN
+  CASE
+    WHEN scaleDenominator > 1000000000 THEN RETURN 0;
+    WHEN scaleDenominator <= 1000000000 AND scaleDenominator > 500000000 THEN RETURN 1;
+    WHEN scaleDenominator <= 500000000 AND scaleDenominator > 200000000 THEN RETURN 2;
+    WHEN scaleDenominator <= 200000000 AND scaleDenominator > 100000000 THEN RETURN 3;
+    WHEN scaleDenominator <= 100000000 AND scaleDenominator > 50000000 THEN RETURN 3;
+    WHEN scaleDenominator <= 50000000 AND scaleDenominator > 25000000 THEN RETURN 4;
+    WHEN scaleDenominator <= 25000000 AND scaleDenominator > 12500000 THEN RETURN 5;
+    WHEN scaleDenominator <= 12500000 AND scaleDenominator > 6500000 THEN RETURN 6;
+    WHEN scaleDenominator <= 6500000 AND scaleDenominator > 3000000 THEN RETURN 7;
+    WHEN scaleDenominator <= 3000000 AND scaleDenominator > 1500000 THEN RETURN 8;
+    WHEN scaleDenominator <= 1500000 AND scaleDenominator > 750000 THEN RETURN 9;
+    WHEN scaleDenominator <= 750000 AND scaleDenominator > 400000 THEN RETURN 10;
+    WHEN scaleDenominator <= 400000 AND scaleDenominator > 200000 THEN RETURN 11;
+    WHEN scaleDenominator <= 200000 AND scaleDenominator > 100000 THEN RETURN 12;
+    WHEN scaleDenominator <= 100000 AND scaleDenominator > 50000 THEN RETURN 13;
+    WHEN scaleDenominator <= 50000 AND scaleDenominator > 25000 THEN RETURN 14;
+    WHEN scaleDenominator <= 25000 AND scaleDenominator > 12500 THEN RETURN 15;
+    WHEN scaleDenominator <= 12500 AND scaleDenominator > 5000 THEN RETURN 16;
+    WHEN scaleDenominator <= 5000 AND scaleDenominator > 2500 THEN RETURN 17;
+    WHEN scaleDenominator <= 2500 AND scaleDenominator > 1500 THEN RETURN 18;
+    WHEN scaleDenominator <= 1500 AND scaleDenominator > 750 THEN RETURN 19;
+    WHEN scaleDenominator <= 750 AND scaleDenominator > 500 THEN RETURN 20;
+    WHEN scaleDenominator <= 500 AND scaleDenominator > 250 THEN RETURN 21;
+    WHEN scaleDenominator <= 250 AND scaleDenominator > 100 THEN RETURN 22;
+    WHEN scaleDenominator <= 100 THEN RETURN 23;
+  END CASE;
+END
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION high_road(scaleDenominator numeric, bbox box3d)
   RETURNS TABLE(geometry geometry, highway character varying, railway character varying, kind text, is_link text, is_tunnel text, is_bridge text, explicit_layer integer) AS
 $$
@@ -171,12 +203,10 @@ DECLARE
 BEGIN
   -- TODO use zoom()
   CASE
-    -- z13
-    WHEN scaleDenominator <= 100000 AND scaleDenominator > 50000 THEN
+    WHEN zoom(scaleDenominator) = 13 THEN
       conditions := 'is_bridge=''no''';
 
-    -- z14+
-    WHEN scaleDenominator <= 50000 THEN
+    WHEN zoom(scaleDenominator) >= 14 THEN
       conditions := 'is_bridge=''no'' AND is_tunnel=''no''';
 
     ELSE
@@ -196,28 +226,22 @@ DECLARE
 BEGIN
   -- TODO use zoom()
   CASE
-    -- z10-
-    WHEN scaleDenominator > 400000 THEN
+    WHEN zoom(scaleDenominator) <= 10 THEN
       tablename := 'highroad_z10';
 
-    -- z11
-    WHEN scaleDenominator <= 400000 AND scaleDenominator > 200000 THEN
+    WHEN zoom(scaleDenominator) = 11 THEN
       tablename := 'highroad_z11';
 
-    -- z12
-    WHEN scaleDenominator <= 200000 AND scaleDenominator > 100000 THEN
+    WHEN zoom(scaleDenominator) = 12 THEN
       tablename := 'highroad_z12';
 
-    -- z13
-    WHEN scaleDenominator <= 100000 AND scaleDenominator > 50000 THEN
+    WHEN zoom(scaleDenominator) = 13 THEN
       tablename := 'highroad_z13';
 
-    -- z14
-    WHEN scaleDenominator <= 50000 AND scaleDenominator > 25000 THEN
+    WHEN zoom(scaleDenominator) = 14 THEN
       tablename := 'highroad_z14';
 
-    -- z15+
-    WHEN scaleDenominator <= 25000 THEN
+    WHEN zoom(scaleDenominator) >= 15 THEN
       tablename := 'highroad_z15plus';
 
     ELSE
