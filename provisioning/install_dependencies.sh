2485d13407fcec4d8447d7444529aaaf15bf5275
Seth Fitzsimmons <seth@mojodna.net>
2014-07-17 09:32:54 -0700

    Install mapnik explicitly to avoid multiple copies

--- provisioning/install_dependencies.sh
+++ provisioning/install_dependencies.sh
@@ -4,7 +4,7 @@ sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
 sudo apt-get install -y nodejs make mapnik-utils nginx git unzip gdal-bin zip
-cd ~/
-npm install tilelive-mapnik tessera carto millstone interp tilelive-blend tilelive-solid
-mkdir -p ~/toner-carto
+
+cd
+npm install mapnik tilelive-mapnik tessera carto millstone interp tilelive-blend tilelive-solid
 mkdir -p ~/toner-carto/shp
