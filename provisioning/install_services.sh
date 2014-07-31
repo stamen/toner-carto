b9856f0cbb3174415042817011d3738c9913a4ca
Brandon Liu <bdon@bdon.org>
2014-07-31 14:16:50 -0700

    fix provisioning and add sentry middleware

--- provisioning/install_services.sh
+++ provisioning/install_services.sh
@@ -5,11 +5,12 @@ set -e
 # sudo mkdir /var/clone
 # sudo ln -s /vagrant/ /var/clone/toner-carto
 
-echo "DATABASE_URL: $DATABASE_URL"
+cd /home/ubuntu && npm install tilelive-cache cors express response-time
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo cp /home/ubuntu/toner-carto/tessera.nginx /etc/nginx/sites-available/
 sudo ln -f -s /etc/nginx/sites-available/tessera.nginx /etc/nginx/sites-enabled/tessera.conf
-sudo cp /home/ubuntu/toner-carto/tessera.conf /etc/init/
+cat /home/ubuntu/toner-carto/tessera.conf | SENTRY_DSN=$SENTRY_DSN envsubst > /home/ubuntu/toner-carto/tessera-env.conf
+sudo cp /home/ubuntu/toner-carto/tessera-env.conf /etc/init/tessera.conf
 pushd ..
-PATH=~/node_modules/.bin:$PATH
+PATH=/home/ubuntu/toner-carto/node_modules/.bin:$PATH
 cd /home/ubuntu/toner-carto && make xml
---
+++ server.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var path = require("path");
+var raven = require("raven");
+
+var tessera = require("tessera"),
+    express = require("express"),
+    cors = require("cors"),
+    responseTime = require("response-time"),
+    tilelive = require("tilelive-cache")(require("tilelive"), {
+      size: process.env.CACHE_SIZE,
+      sources: process.env.SOURCE_CACHE_SIZE
+    });
+
+var app = express();
+
+require("tessera/modules")(tilelive, {});
+
+var config = require("./tessera.json");
+Object.keys(config).forEach(function(prefix) {
+  if (config[prefix].timing !== false) {
+    app.use(prefix, responseTime());
+  }
+
+  if (config[prefix].cors !== false) {
+    app.use(prefix, cors());
+  }
+
+  app.use(raven.middleware.express(process.env.SENTRY_DSN));
+  app.use(prefix, express.static(path.join(__dirname, "node_modules/tessera/public")));
+  app.use(prefix, express.static(path.join(__dirname, "node_modules/tessera/bower_components")));
+  app.use(prefix, tessera(tilelive, config[prefix]));
+});
+
+app.listen(process.env.PORT || 8080, function() {
+  console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
+});
